7545c09f0a49f23517231783fc2c67255866d902
vkaash-sharma <vikasayushsharma@gmail.com>
2022-05-24 20:06:44 -0800

    :construction: v3.1 Html css structuring + js added

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let rows = 100;
 let cols = 26;
 let addressColContainer = document.querySelector(".address-col-cont");
 let addressRowContainer = document.querySelector(".address-row-cont");
+let cellsCont = document.querySelector(".cells-cont");
+let addressBar = document.querySelector(".address-bar");
+
 for(let i = 0 ; i < rows ; i++){
     let addressCol = document.createElement("div");
     addressCol.setAttribute("class" ,"address-col")
@@ -15,3 +18,24 @@ for(let i = 0 ; i < cols ; i++){
     addressRow.innerText =  String.fromCharCode(65 +i);
     addressRowContainer.appendChild(addressRow);
 }
+
+for(let i = 0 ; i < rows ; i++){
+    let rowCont = document.createElement("div");
+    rowCont.setAttribute("class" , "row-cont");
+    for(let j = 0 ; j < cols ; j++){
+        let cell = document.createElement("div");
+        cell.setAttribute("class" , "cell")
+        cell.setAttribute("contenteditable" , "true");
+        rowCont.appendChild(cell);
+        // addListnerForAddressBarDisplay(cell , i , j);
+    }
+    cellsCont.appendChild(rowCont);
+}
+
+// function addListnerForAddressBarDisplay(cell , i , j){
+//     cell.addEventListner("click" ,(e) => {
+//         let rowID =i+1;
+//         let colID = String.fromCharCode(65 + j);
+//         addressBar.value =`${colID}${rowID}`
+//     })
+// }
