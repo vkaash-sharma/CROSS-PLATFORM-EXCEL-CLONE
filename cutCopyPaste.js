19bbc15786115db5f4a8a45f14eb0bbf228c6487
vkaash-sharma <vikasayushsharma@gmail.com>
2022-07-26 19:12:44 -0800

    :construction: copyPasteCut added

---
+++ cutCopyPaste.js
@@ -0,0 +1,97 @@
+let ctrlKey;
+document.addEventListener("keydown" , (e) => {
+    ctrlKey = e.ctrlKey;
+})
+document.addEventListener("keyup" , (e) => {
+    ctrlKey = e.ctrlKey;
+})
+
+for(let i = 0 ; i < rows ; i++) {
+    for(let j = 0 ;j < cols ; j++) {
+        let cell = document.querySelector(`.cell[rid="${i}"][cid="${j}"]`);
+        handleSelectCells(cell);
+    }
+}
+
+let copyBtn = document.querySelector(".copy");
+let cutBtn = document.querySelector(".cut");
+let pasteBtn = document.querySelector(".paste");
+
+let rangeStorage =[];
+function handleSelectCells(cell) {
+    cell.addEventListener("click",(e) => {
+      
+        if(!ctrlKey) return;
+        if(rangeStorage.length >= 2 ) {
+            defaultSelectedCellsUI();
+        }
+
+        cell.style.border ="3px solid #218c74"
+        let rid = Number(cell.getAttribute("rid"));
+        let cid = Number(cell.getAttribute("cid"));
+        rangeStorage.push([rid , cid]);
+    })
+}
+
+function defaultSelectedCellsUI() {
+    for(let i = 0 ; i< rangeStorage.length ; i++) {
+        let cell = document.querySelector(`.cell[rid="${rangeStorage[i][0]}"][cid="${rangeStorage[i][1]}"]`);
+        cell.style.border = "1px solid lightgrey"
+    }
+}
+
+let copyData =[];
+copyBtn.addEventListener("click" , (e) => {
+    let strow = rangeStorage[0][0];
+    let  stcol = rangeStorage[0][1];
+    let endrow = rangeStorage[1][0];
+    let endcol = rangeStorage[1][1];
+
+  for(let i = strow ; i <= endrow ; i++) {
+       let copyRow =[];
+    for(let j = stcol ; j <=endcol ; j++) {
+            let cellProp = sheetDB[i][j];
+            copyRow.push(cellProp);
+    }
+    copyData.push(copyRow);
+  }
+
+   defaultSelectedCellsUI();
+})
+
+pasteBtn.addEventListener("click" ,(e) => {
+
+    if(rangeStorage.length < 2 ) return;
+     
+    let rowDiff = Math.abs(rangeStorage[0][0] - rangeStorage[1][0]);
+    let colDiff = Math.abs(rangeStorage[0][1] - rangeStorage[1][1]);
+
+
+    let address = addressBar.value;
+    let [stRow , stCol] = decodeRIDCIDFromAddress(address);
+
+
+    for(let i = stRow , r = 0 ; i<=stRow+rowDiff ; i++,r++) {
+        for(let j = stCol ,c = 0 ; j <= stCol+colDiff ; j++,c++) {
+            let cell = document.querySelector(`.cell[rid="${i}"][cid="${j}"]`);   
+             if(!cell) continue;
+             let data = copyData[r][c]
+             let cellProp = sheetDB[i][j];
+             cellProp.value = data.value;
+             cellProp.bold = data.bold;
+             cellProp.italic = data.italic;
+             cellProp.underline = data.underline;
+             cellProp.fontSize = data.fontSize;
+             cellProp.fontFamily = data.fontFamily;
+             cellProp.fontColor = data.fontColor;
+             cellProp.BGcolor = data.BGcolor;
+             cellProp.alignment = data.alignment;
+             
+             
+            cell.click()
+
+            
+
+        }
+    }
+})
